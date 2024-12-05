a99b7120077527e5ca61aab7aa450e78876952c4
kirtigalani <kittugalani@gmail.com>
2024-12-05 10:33:32 +0530

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Interfaces
+{
+    interface ICustomer1
+    {
+        void print1();
+    }
+    interface ICustomer2 : ICustomer1
+    {
+        void print2();
+    }
+    public class Customer : ICustomer2
+    {
+        public void print2()
+        {
+            Console.WriteLine("print 2");
+        }
+        public void print1()
+        {
+            Console.WriteLine("print 1");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICustomer1 Cust = new Customer();
+            Cust.print1();
+        }
+    }
+}
